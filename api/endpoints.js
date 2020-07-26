357491ed464129b4294b11f8027b5ba2806f0845
Bijan Kundu <bijankundu025@gmail.com>
2020-07-26 18:25:46 +0530

    :tada: Initial Commit

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,18 +1,18 @@
 module.exports = (req, res) => {
   let endpoints = {
-    all: "http://localhost:3000/v1/all",
-    national: "http://localhost:3000/v1/national",
-    business: "http://localhost:3000/v1/business",
-    sports: "http://localhost:3000/v1/sports",
-    world: "http://localhost:3000/v1/world",
-    politics: "http://localhost:3000/v1/politics",
-    technology: "http://localhost:3000/v1/technology",
-    startup: "http://localhost:3000/v1/startup",
-    entertainment: "http://localhost:3000/v1/entertainment",
-    science: "http://localhost:3000/v1/science",
-    automobile: "http://localhost:3000/v1/automobile",
-    miscellaneous: "http://localhost:3000/v1/miscellaneous",
-    hatke: "http://localhost:3000/v1/hatke",
+    all: "https://inshot.herokuapp.com/v1/all",
+    national: "https://inshot.herokuapp.com/v1/national",
+    business: "https://inshot.herokuapp.com/v1/business",
+    sports: "https://inshot.herokuapp.com/v1/sports",
+    world: "https://inshot.herokuapp.com/v1/world",
+    politics: "https://inshot.herokuapp.com/v1/politics",
+    technology: "https://inshot.herokuapp.com/v1/technology",
+    startup: "https://inshot.herokuapp.com/v1/startup",
+    entertainment: "https://inshot.herokuapp.com/v1/entertainment",
+    science: "https://inshot.herokuapp.com/v1/science",
+    automobile: "https://inshot.herokuapp.com/v1/automobile",
+    miscellaneous: "https://inshot.herokuapp.com/v1/miscellaneous",
+    hatke: "https://inshot.herokuapp.com/v1/hatke",
   };
 
   res.json(endpoints);
